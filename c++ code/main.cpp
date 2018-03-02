d50ad18e0bbc5272dd22395797e2ef3ea79af47f
Timroman1993 <59490332@qq.com>
2018-03-02 13:02:31 +0800

    Initial Commit

---
+++ c++ code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++ code
+//
+//  Created by Zoe on 2018/3/2.
+//  Copyright © 2018年 Zoe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
